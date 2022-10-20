8e7f28692f73c5cdd95856e61c024a89670625b5
Hernan <hernan@jsatch.com>
2022-10-20 12:17:35 -0500

    Listado de carreras implementado

--- src/screens/ListadoCursos/ListadoCursosPage.jsx
+++ src/screens/ListadoCursos/ListadoCursosPage.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import {Container} from 'react-bootstrap'
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
@@ -7,11 +7,24 @@ import FiltroCarrera from "./components/FiltroCarrera"
 import GridCursos from "./components/GridCursos"
 
 const ListadoCursosPage = () => {
+    const [listadoCarreras, setListadoCarreras] = useState([])
+
+    const httpObtenerCarreras = async () => {
+        const resp = await fetch("http://localhost:4444/carreras")
+        const data = await resp.json()
+        console.log(data)
+        setListadoCarreras(data)
+    }
+
+    useEffect(() => {
+        httpObtenerCarreras()
+    }, [])
+
     return <Layout
         makeHeader={ () => <Header titulo="Listado de Cursos" /> }
         makeBody={ 
             () =>  <div>
-                <FiltroCarrera />
+                <FiltroCarrera carreras={ listadoCarreras }/>
                 <GridCursos />
             </div>
         }
--- src/screens/ListadoCursos/components/FiltroCarrera.jsx
+++ src/screens/ListadoCursos/components/FiltroCarrera.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Form } from 'react-bootstrap'
 
-const FiltroCarrera = () => {
+const FiltroCarrera = (props) => {
     return <Form>
         <Form.Group>
             <Form.Label>Carrera</Form.Label>
             <Form.Select>
                 <option>---- Seleccione carrera ----</option>
-                <option>Ingeniería de Sistemas</option>
-                <option>Ingeniería Informática</option>
+                {
+                    props.carreras.map((carrera) => {
+                        return <option>
+                            { carrera.nombre }
+                        </option>
+                    })
+                }
             </Form.Select>
         </Form.Group>
     </Form>
