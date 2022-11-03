2f4bbf9b7c2256281551bece65b709395f6c2b90
Hernan <hernan@jsatch.com>
2022-11-03 12:49:53 -0500

    Listando evaluaciones terminado

--- src/screens/ListadoEvaluaciones/components/ListaEvaluaciones.jsx
+++ src/screens/ListadoEvaluaciones/components/ListaEvaluaciones.jsx
@@ -2,24 +2,27 @@ import React from "react";
 
 import { Card, Button } from 'react-bootstrap'
 
-const ListadoEvaluaciones = () => {
+const ListadoEvaluaciones = (props) => {
     return <div>
-        <Card className="mb-2">
-            <Card.Body>
-                Práctica Calificada 1
-            </Card.Body>
-            <Card.Footer>
-                <Button variant="warning" size="sm">Descargar</Button>
-            </Card.Footer>
-        </Card>
-        <Card>
-            <Card.Body>
-                Práctica Calificada 2
-            </Card.Body>
-            <Card.Footer>
-                <Button variant="warning" size="sm">Descargar</Button>
-            </Card.Footer>
-        </Card>
+        {
+            // EXPRESION javascript
+            (() => {
+                if (props.evaluaciones.length == 0) {
+                    return <div>No hay evaluaciones para este curso</div>
+                }else {
+                    return props.evaluaciones.map((evaluacion) => {
+                        return <Card className="mb-2" key={ evaluacion.id }>
+                            <Card.Body>
+                                { evaluacion.nombre }
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button variant="warning" size="sm">Descargar</Button>
+                            </Card.Footer>
+                        </Card>
+                    })
+                }
+            })()
+        }
     </div>
 }
 
